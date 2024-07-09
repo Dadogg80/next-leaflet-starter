8b73f164128be1f9e558c1a3ebad0ed1d72cdf26
ivo garofalo <ivo.garofalo@gmail.com>
2024-07-09 18:34:34 +0200

    🌐 (index.js): Change default center coordinates   ✨ (index.js): Comment out existing section and add a new section with a Map component

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,10 @@ import Button from '@components/Button';
 
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+const DEFAULT_CENTER = [41.414742, 19.720561]
 
 export default function Home() {
+
   return (
     <Layout>
       <Head>
@@ -19,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Section>
+{/*       <Section>
         <Container>
           <h1 className={styles.title}>
             Next.js Leaflet Starter
@@ -49,7 +50,32 @@ export default function Home() {
             <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
           </p>
         </Container>
-      </Section>
+      </Section> */}
+      <Section>
+
+        
+
+
+  <Container >
+  <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={8}>
+            {({ TileLayer, Marker, Popup }) => (
+              <>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+                <Marker position={DEFAULT_CENTER}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+              </>
+            )}
+          </Map>
+
+)
+        </Container>
+        </Section>
     </Layout>
   )
 }

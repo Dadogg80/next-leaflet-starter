a3e158131292fa9b3e4b9e1c46d068223a82fca2
Ivo G <41997352+Dadogg80@users.noreply.github.com>
2024-07-09 20:00:31 +0000

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import Head from 'next/head';
 
 import Layout from '@components/Layout';
 import Section from '@components/Section';
 import Container from '@components/Container';
 import Map from '@components/Map';
-import Button from '@components/Button';
 
 import styles from '@styles/Home.module.scss';
 
-const DEFAULT_CENTER = [41.414742, 19.720561]
-
 export default function Home() {
+  // Initialize state for latitude and longitude
+  const [latitude, setLatitude] = useState(41.87194);
+  const [longitude, setLongitude] = useState(12.56738);
+
+  // Event handler for latitude input field
+  const handleLatitudeChange = (e) => {
+    setLatitude(parseFloat(e.target.value));
+  }
+
+  // Event handler for longitude input field
+  const handleLongitudeChange = (e) => {
+    setLongitude(parseFloat(e.target.value));
+  }
 
   return (
     <Layout>
@@ -19,63 +30,51 @@ export default function Home() {
         <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-{/*       <Section>
-        <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter
-          </h1>
-
-          <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
-              <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
-              </>
-            )}
-          </Map>
-
-          <p className={styles.description}>
-            <code className={styles.code}>npx create-next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-          </p>
-
-          <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
-          </p>
-        </Container>
-      </Section> */}
-      <Section>
-
-        
-
-
-  <Container >
-  <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={8}>
-            {({ TileLayer, Marker, Popup }) => (
-              <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
-              </>
-            )}
-          </Map>
-
-)
-        </Container>
+      <div>
+        <div className={styles.title}>
+          <input 
+            type="number" 
+            id="latitude" 
+            name="latitude" 
+            placeholder="Latitude" 
+            value={latitude} 
+            onChange={handleLatitudeChange} 
+          />
+          <input 
+            type="number" 
+            id="longitude" 
+            name="longitude" 
+            placeholder="Longitude" 
+            value={longitude} 
+            onChange={handleLongitudeChange} 
+          />
+        </div>
+        <Section>
+          <Container>
+            <Map 
+              className={styles.homeMap} 
+              width="800" 
+              height="400" 
+              center={[latitude, longitude]} 
+              zoom={8}
+            >
+              {({ TileLayer, Marker, Popup }) => (
+                <>
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                  />
+                  <Marker position={[latitude, longitude]}>
+                    <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                  </Marker>
+                </>
+              )}
+            </Map>
+          </Container>
         </Section>
+      </div>
     </Layout>
   )
 }
